ffffd1ead6f734f75ff75ff8da48a7e63f02bcd7
EmiliyanG6 <emiliyan.geshkov.a65@learn.telerikacademy.com>
2025-02-15 17:13:28 +0200

    editing Profile added

--- Carpooling/src/main/java/com/carpooling/carpooling/controllers/mvc/ProfileMvcController.java
+++ Carpooling/src/main/java/com/carpooling/carpooling/controllers/mvc/ProfileMvcController.java
@@ -1,22 +1,25 @@
 package com.carpooling.carpooling.controllers.mvc;
 
+import com.carpooling.carpooling.exceptions.AuthorizationException;
 import com.carpooling.carpooling.helpers.AuthenticationHelper;
 import com.carpooling.carpooling.helpers.TravelMapper;
+import com.carpooling.carpooling.helpers.UserMapper;
 import com.carpooling.carpooling.models.Dtos.FeedbackDto;
 import com.carpooling.carpooling.models.Dtos.TravelDto;
+import com.carpooling.carpooling.models.Dtos.UserDto;
 import com.carpooling.carpooling.models.Travel;
 import com.carpooling.carpooling.models.User;
 import com.carpooling.carpooling.services.interfaces.FeedbackService;
 import com.carpooling.carpooling.services.interfaces.TravelService;
 import com.carpooling.carpooling.services.interfaces.UserService;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.http.HttpHeaders;
 import java.security.Principal;
@@ -32,18 +35,21 @@ public class ProfileMvcController {
     private final TravelMapper travelMapper;
     private final AuthenticationHelper authenticationHelper;
     private final FeedbackService feedbackService;
+    private final UserMapper userMapper;
 
     @Autowired
     public ProfileMvcController(UserService userService,
                                 TravelService travelService,
                                 AuthenticationHelper authenticationHelper,
                                 TravelMapper travelMapper,
-                                FeedbackService feedbackService) {
+                                FeedbackService feedbackService,
+                                UserMapper userMapper) {
         this.userService = userService;
         this.travelService = travelService;
         this.authenticationHelper = authenticationHelper;
         this.travelMapper = travelMapper;
         this.feedbackService = feedbackService;
+        this.userMapper = userMapper;
     }
 
 
@@ -65,5 +71,52 @@ public class ProfileMvcController {
 
         return "ProfileView";
 
+    }
+
+    @GetMapping("/{id}/edit")
+    public String showEditProfileForm(@PathVariable long id, Model model, HttpSession session) {
+        try {
+            authenticationHelper.tryGetCurrentUser(session);
+        } catch (Exception e) {
+            return "redirect:/auth/login";
+        }
+        User user = userService.getUserById(id);
+        UserDto userDto = userMapper.toDto(user);
+        model.addAttribute("userId", id);
+        model.addAttribute("user", userDto);
+        return "ProfileEditView";
+    }
+
+    @PostMapping("/{id}/edit")
+    public String updateUser(@PathVariable long id,
+                             @Valid @ModelAttribute("user") UserDto userDto,
+                             BindingResult bindingResult,
+                             Model model,
+                             HttpSession session) {
+        User currentUser;
+
+        try {
+            currentUser = authenticationHelper.tryGetCurrentUser(session);
+            if (currentUser.getId() != id) {
+                return "redirect:/profile";
+            }
+        } catch (Exception e) {
+            return "redirect:/auth/login";
+        }
+
+        if (bindingResult.hasErrors()) {
+            return "ProfileEditView";
+        }
+
+        try {
+            User userToUpdate = userMapper.toEntity(userDto);
+            userService.update(id, userToUpdate);
+            model.addAttribute("currentUser",currentUser);
+            model.addAttribute("success", "Changed user successfully");
+            return "redirect:/profile";
+        } catch (AuthorizationException e) {
+            model.addAttribute("error", "An error occurred while updating the profile.");
+            return "ErrorView";
+        }
     }
 }
--- Carpooling/src/main/java/com/carpooling/carpooling/helpers/UserMapper.java
+++ Carpooling/src/main/java/com/carpooling/carpooling/helpers/UserMapper.java
@@ -1,6 +1,7 @@
 package com.carpooling.carpooling.helpers;
 
 import com.carpooling.carpooling.models.Dtos.RegisterDto;
+import com.carpooling.carpooling.models.Dtos.UserDto;
 import com.carpooling.carpooling.models.User;
 import org.springframework.stereotype.Component;
 
@@ -17,6 +18,25 @@ public class UserMapper {
         user.setPhoneNumber(registerDto.getPhoneNumber());
         return user;
     }
+    public UserDto toDto(User user){
+        UserDto userDto = new UserDto();
+        userDto.setUsername(user.getUsername());
+        userDto.setEmail(user.getEmail());
+        userDto.setFirstName(user.getFirstName());
+        userDto.setLastName(user.getLastName());
+        userDto.setPhoneNumber(user.getPhoneNumber());
+        return userDto;
+    }
 
 
+    public User toEntity(UserDto userDto) {
+        User user = new User();
+        user.setId(userDto.getId());
+        user.setUsername(userDto.getUsername());
+        user.setEmail(userDto.getEmail());
+        user.setFirstName(userDto.getFirstName());
+        user.setLastName(userDto.getLastName());
+        user.setPhoneNumber(userDto.getPhoneNumber());
+        return user;
+    }
 }
